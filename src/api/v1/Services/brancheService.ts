ea84b83357fea8bdd26f2e9107d8990b30827681
DIMPLE <Ddimple@rrc.ca>
2025-10-01 14:37:36 -0500

    Feat: Define the services CRUD for employee data.

---
+++ src/api/v1/Services/brancheService.ts
@@ -0,0 +1,28 @@
+import {Branch, branches} from "../../../data/branches";
+
+export const getAllbranches = (): Branch[] => {
+    return branches;
+};
+
+export const createBranch = (branch: Branch): string => {
+    branches.push(branch);
+    return "Branch added successfully."
+};
+
+export const updateBranch = (id: number, updatedBranch: Branch): string => {
+    const index = branches.findIndex(branches => branches.id === id);
+    if (index !== -1) {
+        branches[index] = updatedBranch;
+        return "Branch updated";
+    }
+    return "Branch not found";
+};
+
+export const deleteBranch = (id: number): string => {
+    const index = branches.findIndex(branches => branches.id === id);
+    if (index !== -1) {
+        branches.splice(index, 1);
+        return "Branch deleted";
+    }
+    return "Branch not found";
+};
---
+++ src/api/v1/Services/employeeService.ts
@@ -0,0 +1,32 @@
+import {employees, Employee } from "../../../data/employees";
+
+export const getAllEmployees = (): Employee[] => {
+    return employees;
+};
+
+export const createEmployee = (employee: Omit<Employee, "id">): Employee => {
+    const newEmployee: Employee = {
+        id: employees.length + 1, // simple ID generation
+        ...employee,
+    };
+    employees.push(newEmployee);
+    return newEmployee;
+};
+
+export const updateEmployee = (id: number, updatedEmployee: Partial<Employee>): Employee | null => {
+    const index = employees.findIndex(emp => emp.id === id);
+    if (index !== -1) {
+        employees[index] = { ...employees[index], ...updatedEmployee};
+        return employees[index];
+    }
+    return null;
+};
+
+export const deleteEmployee = (id: number): boolean => {
+    const index = employees.findIndex (emp => emp.id === id);
+    if (index !== -1) {
+        employees.splice(index, 1);
+        return true;
+    }
+    return false;
+};
