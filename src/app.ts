e5e508c3c752cef31b93c348e01f1c42047d59e1
DIMPLE <Ddimple@rrc.ca>
2025-10-12 16:08:05 -0500

    Remove the direct access to app.ts when server run and also remove the duplicate routes.

--- src/app.ts
+++ src/app.ts
@@ -17,12 +17,6 @@ app.get("/health", (req, res) => {
     res.send("Server is healthy");
 });
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
 // employee Endpoints route
 app.use("/api/v1/employees", routes)
 
